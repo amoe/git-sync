37c7c2ab7d33fb5439de922ec4d9bc779f52369f
David Banks <amoebae@gmail.com>
2019-10-07 01:30:02 +0100

    refactor

--- git_sync.py
+++ git_sync.py
@@ -12,35 +12,44 @@ GIT_DIR = "/home/amoe/ng-annex"
 AUTO_COMMIT_MESSAGE = 'automatic commit'
 PRIVILEGED_SEGMENT_BINARY_NAME = '/usr/local/share/git-sync/privileged-part'
 
-log_stream = open(LOG_PATH, 'a')
-
-logging.basicConfig(
-    handlers=[
-        logging.StreamHandler(sys.stdout),
-        logging.StreamHandler(log_stream)
-    ],
-    level=logging.DEBUG,            
-    format="%(asctime)s - %(levelname)8s - %(name)s - %(message)s"
-)
-
-
 def git(rest_of_command):
     prefix = ['git', '-C', GIT_DIR]
     prefix.extend(rest_of_command)
     return prefix
 
-status_output = subprocess.check_output(git(['status', '--porcelain=v1']))
 
-# If status output is blank, then nothing has changed.
-if status_output:
-    subprocess.check_call(git(['add', '-A']), stdout=log_stream, stderr=log_stream)
-    subprocess.check_call(git(['commit', '-m', AUTO_COMMIT_MESSAGE]), stdout=log_stream, stderr=log_stream)
-else:
-    info("No changes, not committing.")
+class GitSync(object):
+    def run(self):
+        log_stream = open(LOG_PATH, 'a')
+
+        logging.basicConfig(
+            handlers=[
+                logging.StreamHandler(sys.stdout),
+                logging.StreamHandler(log_stream)
+            ],
+            level=logging.DEBUG,            
+            format="%(asctime)s - %(levelname)8s - %(name)s - %(message)s"
+        )
+
+        status_output = subprocess.check_output(git(['status', '--porcelain=v1']))
+
+        # If status output is blank, then nothing has changed.
+        if status_output:
+            subprocess.check_call(git(['add', '-A']), stdout=log_stream, stderr=log_stream)
+            subprocess.check_call(git(['commit', '-m', AUTO_COMMIT_MESSAGE]), stdout=log_stream, stderr=log_stream)
+        else:
+            info("No changes, not committing.")
 
 
-subprocess.check_call(['sudo', PRIVILEGED_SEGMENT_BINARY_NAME, 'fetch'], stdout=log_stream, stderr=log_stream)
-subprocess.check_call(git(['merge', '--ff', '--ff-only']), stdout=log_stream, stderr=log_stream)
-subprocess.check_call(['sudo', PRIVILEGED_SEGMENT_BINARY_NAME, 'push'], stdout=log_stream, stderr=log_stream)
+        subprocess.check_call(['sudo', PRIVILEGED_SEGMENT_BINARY_NAME, 'fetch'], stdout=log_stream, stderr=log_stream)
+        subprocess.check_call(git(['merge', '--ff', '--ff-only']), stdout=log_stream, stderr=log_stream)
+        subprocess.check_call(['sudo', PRIVILEGED_SEGMENT_BINARY_NAME, 'push'], stdout=log_stream, stderr=log_stream)
 
-log_stream.close()
+        log_stream.close()
+
+try:
+    obj = GitSync()
+    obj.run()
+except e:
+    message = str(e).encode('utf-8')
+    subprocess.run(['cron-notify-send', 'git-sync'], input=message)    
